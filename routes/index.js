601a0cbe64d3cace4e2d0fcb5cfbb711d9aca06c
Ivor King <ivorking@gmail.com>
2019-02-19 08:39:26 +0000

    updates - reviewing

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const storeController = require('../controllers/storeController');
+// const storeController = require('../controllers/storeController');
 
-// Do work here
-router.get('/', storeController.homePage);
+// // Do work here
+// router.get('/', storeController.homePage);
+
+// module.exports = router;
+
+router.get('/', (req, res) => {
+    const wes = { name: 'Wes', age: 100, cool: false };
+    // res.json(req.query);
+    res.render('hello.pug');
+})
+
+router.get('/reverse/:name', (req, res) => {
+    const reverse = [...req.params.name].reverse().join('')
+    res.send(reverse);
+});
 
 module.exports = router;

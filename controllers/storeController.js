246f7646ae1ac460915fd73a08dc60e93749f729
Ivor King <ivorking@gmail.com>
2019-06-16 22:07:08 +0100

    update

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -18,13 +18,30 @@ exports.addStore = (req,res) => {
 };
 
 exports.createStore = async (req, res) => {
-   const store = await (new Store(req.body).save());
+   const store = await (new Store(req.body)).save();
    req.flash('success', `Successfully Created ${store.name}. care for review?`);
    res.redirect(`/store/${store.slug}`);
 };
 
 exports.getStores = async (req, res) => {
    const stores = await Store.find();
-   console.log(stores);
-   res.render('stores', { title: 'Stores', stores } );
+   res.render('stores', { title: 'Stores', stores });
+};
+
+exports.editStore = async ( req, res ) => {
+   res.json(req.params);
+   const store = await Store.findOne({ _id: req.params.id });
+   res.json(store)
+   res.render('editStore', { title: `Edit ${store.name}`, store: store})
+};
+
+exports.updateStore = async (req.res) => {
+   const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, 
+      {
+      new: true, // return new instead of old store
+      runValidators: true
+      }).exec();
+      req.flash('success', `Successfully updasted <strong>${store.name}</strong>.
+      <a href="/stores/${store.slug}">View store -></a>`);
+   res.redirect(`/stores/${store._id}/edit`);
 };
